483124bc5ade2fc4726ea3c6294703d3384b0475
kminzy <kminzy0704@gmail.com>
2022-02-15 13:54:04 +0900

    import, export

---
+++ [week_06] 백엔드 Ⅰ/[0215 Theory]/processing.js
@@ -0,0 +1,8 @@
+import { kor, eng, math } from './score.js';
+import { grade } from './score.js';
+
+console.log(kor);
+console.log(eng);
+console.log(math);
+
+console.log(grade);
---
+++ [week_06] 백엔드 Ⅰ/[0215 Theory]/score.js
@@ -0,0 +1,10 @@
+// 학생의 성적
+export const kor = 90
+export const eng = 60
+export const math = 70
+
+export const grade = {
+  kor: 90,
+  eng: 60,
+  math: 70
+}
