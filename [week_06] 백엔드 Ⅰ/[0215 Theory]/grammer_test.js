4430235fc74ca76dba56ec4ee9903f9557a67f7e
kminzy <kminzy0704@gmail.com>
2022-02-15 12:01:48 +0900

    javascript 복습 및 이름 생성기

---
+++ [week_06] 백엔드 Ⅰ/[0215 Theory]/grammer_test.js
@@ -0,0 +1,26 @@
+/*
+1. var
+var korean = 90
+console.log(korean)
+
+korean = 100
+console.log(korean)
+*/
+
+/*
+2. let
+let korean = 90
+console.log(korean)
+
+korean = 100
+console.log(korean)
+*/
+
+/*
+3. const
+const korean = 90
+console.log(korean)
+
+korean = 100
+console.log(korean)
+*/
