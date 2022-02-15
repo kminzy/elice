33cb13a079b855367dd9a60d95997f671beaa5e8
kminzy <kminzy0704@gmail.com>
2022-02-15 14:26:05 +0900

    Node.js exports / require

--- [week_06] 백엔드 Ⅰ/[0215 Theory]/processing.js
+++ [week_06] 백엔드 Ⅰ/[0215 Theory]/processing.js
@@ -1,8 +1,13 @@
-import { kor, eng, math } from './score.js';
-import { grade } from './score.js';
+// import { kor, eng, math } from './score.js';
+// import { grade } from './score.js';
 
-console.log(kor);
-console.log(eng);
-console.log(math);
+const score = require('./score.js')
 
-console.log(grade);
+// console.log(kor);
+// console.log(eng);
+// console.log(math);
+
+console.log(score);
+console.log(score.kor);
+console.log(score.math);
+console.log(score.myfoo());
--- [week_06] 백엔드 Ⅰ/[0215 Theory]/score.js
+++ [week_06] 백엔드 Ⅰ/[0215 Theory]/score.js
@@ -1,10 +1,18 @@
 // 학생의 성적
-export const kor = 90
-export const eng = 60
-export const math = 70
+const kor = 90
+const eng = 60
+const math = 70
 
-export const grade = {
-  kor: 90,
-  eng: 60,
-  math: 70
+// export const grade = {
+//   kor: 90,
+//   eng: 60,
+//   math: 70
+// }
+
+exports.kor = kor
+exports.eng = eng
+exports.math = math
+exports.myfoo = function() {
+  //console.log('Hello World');
+  return'Hello World';
 }
