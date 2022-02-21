9cef4c309fe25db461e297c1cbb15f4fe1345dbf
kminzy <kminzy0704@gmail.com>
2022-02-21 16:44:01 +0900

    today service completed

--- [week_06] 백엔드 Ⅰ/[2017 Theory] ExpressJs_Basic/app.js
+++ [week_06] 백엔드 Ⅰ/[2017 Theory] ExpressJs_Basic/app.js
@@ -11,6 +11,31 @@ const pickMyFood = () => {
   return food[idx]
 }
 
+const mbti = ['INTJ - 용의주도한 전략가 (Architect)',
+  'INTP - 논리적인 사색가 (Logician)',
+  'ENTJ - 대담한 통솔자 (Commander)',
+  'ENTP - 뜨거운 논쟁을 즐기는 변론가 (Debater)',
+  'INFJ - 선의의 옹호자 (Advocate)',
+  'INFP - 열정적인 중재자 (Mediator)',
+  'ENFJ - 정의로운 사회운동가 (Protagonist)',
+  'ENFP - 재기발랄한 활동가 (Campaigner)',
+  'ISTJ - 청렴결백한 논리주의자 (Logistician)',
+  'ISFJ - 용감한 수호자 (Defender)',
+  'ESTJ - 엄격한 관리자 (Executive)',
+  'ESFJ - 사교적인 외교관 (Consul)',
+  'ISTP - 만능 재주꾼 (Virtuoso)',
+  'ISFP - 호기심 많은 예술가 (Adventurer)',
+  'ESTP - 모험을 즐기는 사업가 (Entrepreneur)',
+  'ESFP - 자유로운 영혼의 연예인 (Entertainer)'
+]
+
+const direction = ['동', '서', '남', '북']
+const reward = ['우정', '행운', '재물', '지혜', '즐거움', '놀라움']
+
+const randIdx = function(arr) {
+  return Math.floor(Math.random() * arr.length)
+}
+
 app.use('/', indexRoute)
 
 // app.get('/', (req, res) => {
@@ -20,6 +45,13 @@ app.use('/', indexRoute)
 app.get('/food', (req, res) => {
   text = pickMyFood()
   res.send(`오늘 당신에게 추천드리는 메뉴는 바로 ${text} 입니다!!`)
+})
+
+app.get('/lucky', (req, res) => {
+  res.send(`<p>오늘 당신이 <font color="blue">${direction[randIdx(direction)]}</font> 쪽으로 가신다면,</p>
+  <p>당신이 만날 귀인은 바로 <font color="purple">${mbti[randIdx(mbti)]}</font> 입니다.</p>
+  <p>그는 당신에게 <font color="red">${reward[randIdx(reward)]}</font> 을(를) 선사할 것입니다.</p>
+  <p>그는 당신에게 소중한 인연입니다.</p>`)
 })
 
 app.get('/webtoon', (req, res) => {
