0cf81bd0f643e36a1fdb577c643bfdd19f7cc21d
kminzy <kminzy0704@gmail.com>
2022-02-21 16:07:31 +0900

    express start

--- [week_06] 백엔드 Ⅰ/[2017 Theory] ExpressJs_Basic/app.js
+++ [week_06] 백엔드 Ⅰ/[2017 Theory] ExpressJs_Basic/app.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello, world')
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/webtoon', (req, res) => {
+  res.send('<h2>현재 준비중입니다..ㅠㅠ 조금만 기다려 주세요. under construction...</h2>')
+})
+
+app.get('/profile', (req, res) => {
+  res.send('<h2>별명: 멋쟁이, 취미: 별스타그램.. </h2>')
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).send(err.message)
 })
 
 app.listen(3000, () => {
