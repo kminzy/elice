112652dd1f15f62e22ba7f826bdb6b1a2dd03241
kminzy <kminzy0704@gmail.com>
2022-02-24 14:58:55 +0900

    my diary complete

--- [week_07] 백엔드 Ⅱ/[0224 Theory (2)] JWT_Diary/routes/index.js
+++ [week_07] 백엔드 Ⅱ/[0224 Theory (2)] JWT_Diary/routes/index.js
@@ -4,16 +4,16 @@ const User = require('../schemas/user');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.find({});
-    console.log(users)
-    // res.send("Hello, world.")
-    // console.log(res.render)
-    res.render('mongoose', { users });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+    try {
+        const users = await User.find({});
+        console.log(users)
+        // res.send("Hello, world.")
+        // console.log(res.render)
+        res.render('mongoose', { users });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
 });
 
 module.exports = router;
