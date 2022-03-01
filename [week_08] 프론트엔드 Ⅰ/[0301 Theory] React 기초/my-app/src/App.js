040f68b75aba35b7dbb6953414d49de01f579ef4
kminzy <kminzy0704@gmail.com>
2022-03-01 11:11:14 +0900

    react 기본 코드 setting

---
+++ [week_08] 프론트엔드 Ⅰ/[0301 Theory] React 기초/my-app/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+    return (
+      <div>
+        <header>
+          <h1><a href="/">WEB</a></h1>
+        </header>
+        <nav>
+          <ol>
+            <li><a href="/read/1">html</a></li>
+            <li><a href="/read/2">css</a></li>
+            <li><a href="/read/3">js</a></li>
+          </ol>
+        </nav>
+        <article>
+          <h2>WELCOME</h2>
+          Hello, React!!!
+        </article>
+      </div>
+    );
+  }
+
+export default App;
