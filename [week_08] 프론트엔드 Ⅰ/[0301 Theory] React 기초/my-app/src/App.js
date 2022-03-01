f1b957d86f5919db0892733e009a35f2bf958962
kminzy <kminzy0704@gmail.com>
2022-03-01 11:38:39 +0900

    컴포넌트화

--- [week_08] 프론트엔드 Ⅰ/[0301 Theory] React 기초/my-app/src/App.js
+++ [week_08] 프론트엔드 Ⅰ/[0301 Theory] React 기초/my-app/src/App.js
@@ -1,24 +1,50 @@
 import "./App.css";
 
+function HeaderTag() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>WELCOME</h2>
+      Hello, React!!!
+    </article>
+  );
+}
+
 function App() {
-    return (
-      <div>
-        <header>
-          <h1><a href="/">WEB</a></h1>
-        </header>
-        <nav>
-          <ol>
-            <li><a href="/read/1">html</a></li>
-            <li><a href="/read/2">css</a></li>
-            <li><a href="/read/3">js</a></li>
-          </ol>
-        </nav>
-        <article>
-          <h2>WELCOME</h2>
-          Hello, React!!!
-        </article>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <HeaderTag></HeaderTag>
+      <Nav />
+      <Article />
+    </div>
+  );
+}
 
 export default App;
