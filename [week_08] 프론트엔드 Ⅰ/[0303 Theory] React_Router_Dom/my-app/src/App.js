7738b961ea5616d39716bdb9accaeeaebad008d5
kminzy <kminzy0704@gmail.com>
2022-03-03 14:56:28 +0900

    Update 기능 추가

--- [week_08] 프론트엔드 Ⅰ/[0303 Theory] React_Router_Dom/my-app/src/App.js
+++ [week_08] 프론트엔드 Ⅰ/[0303 Theory] React_Router_Dom/my-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Link, Routes, Route, useParams } from "react-router-dom";
+import { Link, Routes, Route, useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Article(props) {
@@ -82,9 +82,62 @@ function Create({ onCreate }) {
   );
 }
 
+function Update(props) {
+  const params = useParams();
+  const id = Number(params.id);
+  let _title,
+    _body = "";
+  for (let i = 0; i < props.topics.length; i++) {
+    if (props.topics[i].id === id) {
+      _title = props.topics[i].title;
+      _body = props.topics[i].body;
+    }
+  }
+
+  const [title, setTitle] = useState(_title);
+  const [body, setBody] = useState(_body);
+
+  function SubmitHandler(event) {
+    event.preventDefault();
+    const t = event.target.title.value;
+    const b = event.target.body.value;
+    props.onUpdate(id, t, b);
+  }
+
+  return (
+    <article>
+      <h1>Update</h1>
+      <form onSubmit={SubmitHandler}>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          ></input>
+        </p>
+        <p>
+          <textarea
+            name="body"
+            placeholder="Body"
+            value={body}
+            onChange={(event) => setBody(event.target.value)}
+          ></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Update"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   console.log("App render");
-
+  const navigate = useNavigate(); // Navigate라는 함수를 사용할 수 있게 된다.
   let [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
@@ -98,7 +151,20 @@ function App() {
     const newTopic = { id: nextId, title: _title, body: _body };
     newTopics.push(newTopic);
     setTopics(newTopics);
+    navigate("/read/" + nextId); // 인자로 전달된 주소로 redirection 된다.
     setNextId(nextId + 1);
+  }
+
+  function UpdateHandler(_id, _title, _body) {
+    const newTopics = [...topics];
+    for (let i = 0; i < newTopics.length; i++) {
+      if (newTopics[i].id === _id) {
+        newTopics[i].title = _title;
+        newTopics[i].body = _body;
+      }
+    }
+    setTopics(newTopics);
+    navigate("/read/" + _id); // 인자로 전달된 주소로 redirection 된다.
   }
 
   return (
@@ -116,19 +182,34 @@ function App() {
           path="/create"
           element={<Create onCreate={CreateHandler}></Create>}
         ></Route>
+        <Route
+          path="/update/:id"
+          element={<Update topics={topics} onUpdate={UpdateHandler}></Update>}
+        ></Route>
       </Routes>
-      <ul>
-        <li>
-          <Link to="/create">Create</Link>
-        </li>
-        <li>
-          <Link to="/update">Update</Link>
-        </li>
-        <li>
-          <input type="button" value="Delete" />
-        </li>
-      </ul>
+      <Routes>
+        <Route path="/" element={<Control></Control>}></Route>
+        <Route path="/read/:id" element={<Control></Control>}></Route>
+      </Routes>
     </>
+  );
+}
+
+function Control() {
+  const params = useParams();
+  console.log("params:", params);
+  return (
+    <ul>
+      <li>
+        <Link to="/create">Create</Link>
+      </li>
+      <li>
+        <Link to={"/update/" + params.id}>Update</Link>
+      </li>
+      <li>
+        <input type="button" value="Delete" />
+      </li>
+    </ul>
   );
 }
 
