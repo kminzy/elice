fa57f4f8001bd3d7516938b8530c0b6bf51f0c70
kminzy <kminzy0704@gmail.com>
2022-03-09 16:46:38 +0900

    간단한 JSON CRD, 명세서 작성

--- [week_09] 백엔드 보강/[0309] BackEnd_Review/elice-0309/app.js
+++ [week_09] 백엔드 보강/[0309] BackEnd_Review/elice-0309/app.js
@@ -1,8 +1,56 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/data", (req, res) => {
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    const _data = JSON.parse(data);
+    console.log(_data);
+    res.send(_data);
+  });
+});
+
+// 클라이언트에서 오는 모든 정보는 req에 담겨있다.
+app.get("/data/:id", (req, res) => {
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    const _data = JSON.parse(data);
+    const findData = _data.find(
+      (element) => element.id === Number(req.params.id)
+    );
+    console.log(findData);
+    res.send(findData);
+  });
+});
+
+app.post("/data", (req, res) => {
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    const _data = JSON.parse(data);
+
+    _data.push(req.body);
+
+    fs.writeFile("data.json", JSON.stringify(_data, null, 5), (err, data2) => {
+      res.send(_data);
+    });
+  });
+});
+
+app.delete("/data/:id", (req, res) => {
+  fs.readFile("data.json", "utf-8", (err, data) => {
+    let _data = JSON.parse(data);
+
+    _data = _data.filter((elem) => elem.id !== Number(req.params.id));
+
+    fs.writeFile("data.json", JSON.stringify(_data, null, 5), (err, data2) => {
+      res.send(_data);
+    });
+  });
 });
 
 app.listen(3000, () => console.log("listen 3000"));
